e563d1b26bc63092aa9341d01cb9b59735f7e21b
Alpvax <alpvax@netscape.net>
2016-05-13 14:31:32 +0100

    Improved attachCapabilities, checks for capability instead of key

--- src/main/java/alpvax/characteroverhaul/core/CharacterOverhaulHooks.java
+++ src/main/java/alpvax/characteroverhaul/core/CharacterOverhaulHooks.java
@@ -1,11 +1,16 @@
 package alpvax.characteroverhaul.core;
 
-import static alpvax.characteroverhaul.api.Reference.CAPABILITY_CHARACTER_KEY;
+import java.util.ArrayList;
+import java.util.List;
 
+import alpvax.characteroverhaul.api.Reference;
 import alpvax.characteroverhaul.api.character.CharacterBase;
 import alpvax.characteroverhaul.api.character.ICharacter;
 import alpvax.characteroverhaul.capabilities.CapabilityCharacterHandler;
 import alpvax.characteroverhaul.capabilities.SerializeableCapabilityProvider;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -16,10 +21,24 @@ public class CharacterOverhaulHooks
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public void attachCapabilities(AttachCapabilitiesEvent.Entity event)
 	{
-		if(!event.getEntity().hasCapability(CapabilityCharacterHandler.CHARACTER_CAPABILITY, null) && !event.getCapabilities().keySet().contains(CAPABILITY_CHARACTER_KEY))
+		if(!hasCapability(event, CapabilityCharacterHandler.CHARACTER_CAPABILITY, null))
 		{
-			event.addCapability(CAPABILITY_CHARACTER_KEY, new SerializeableCapabilityProvider<ICharacter>(new CharacterBase(event.getEntity()), CapabilityCharacterHandler.CHARACTER_CAPABILITY));
+			event.addCapability(Reference.CAPABILITY_CHARACTER_KEY, new SerializeableCapabilityProvider<ICharacter>(new CharacterBase(event.getEntity()), CapabilityCharacterHandler.CHARACTER_CAPABILITY));
 		}
+	}
+
+	private boolean hasCapability(AttachCapabilitiesEvent event, Capability<?> capability, EnumFacing facing)
+	{
+		List<ICapabilityProvider> list = new ArrayList<>(event.getCapabilities().values());
+		list.add(0, (ICapabilityProvider)event.getObject());//Insert event object as first element.
+		for(ICapabilityProvider p : list)
+		{
+			if(p.hasCapability(capability, facing))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@SubscribeEvent
