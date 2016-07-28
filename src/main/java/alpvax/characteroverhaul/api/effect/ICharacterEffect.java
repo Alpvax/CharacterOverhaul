ae6475f658fbf6dc6a62286350bb3e5e2d11607c
Alpvax <alpvax@netscape.net>
2016-07-28 22:32:06 +0100

    Made effects seperate instances for each character.

--- src/main/java/alpvax/characteroverhaul/api/effect/ICharacterEffect.java
+++ src/main/java/alpvax/characteroverhaul/api/effect/ICharacterEffect.java
@@ -1,19 +1,31 @@
 package alpvax.characteroverhaul.api.effect;
 
-import alpvax.characteroverhaul.api.character.ICharacter;
+import java.util.UUID;
 
 public interface ICharacterEffect
 {
+	public UUID getId();
+
 	/**
 	 * If this returns true, this effect will have to be equipped in an ability slot for it to have an effect.<br>
 	 * Otherwise it will appear on the effects screen instead and will have an effect without being equipped.
 	 */
-	public boolean requiresAbilitySlot(ICharacter character);
+	public boolean requiresAbilitySlot();
 
 	/**
 	 * Called every tick, use it to affect the character or other objects around the character.
 	 */
-	public void tick(ICharacter character);
+	public void tick();
 
 	public IEffectProvider getProvider();
+
+	/**
+	 * Called when the effect is added to the character.
+	 */
+	public void onAttach();
+
+	/**
+	 * Called when the effect is removed from the character.
+	 */
+	public void onRemove();
 }
