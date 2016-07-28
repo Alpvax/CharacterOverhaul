9624f77d5b5be0382d85775fee31c3e65ebc01fc
Alpvax <alpvax@netscape.net>
2016-07-28 22:32:47 +0100

    Added character effect ticking

--- src/main/java/alpvax/characteroverhaul/core/CharacterOverhaulHooks.java
+++ src/main/java/alpvax/characteroverhaul/core/CharacterOverhaulHooks.java
@@ -8,10 +8,13 @@ import org.lwjgl.input.Mouse;
 import alpvax.characteroverhaul.api.Reference;
 import alpvax.characteroverhaul.api.character.CharacterBase;
 import alpvax.characteroverhaul.api.character.ICharacter;
+import alpvax.characteroverhaul.api.effect.ICharacterEffect;
 import alpvax.characteroverhaul.capabilities.CapabilityCharacterHandler;
 import alpvax.characteroverhaul.capabilities.SerializeableCapabilityProvider;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.InventoryEffectRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.client.event.GuiScreenEvent.DrawScreenEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.MouseInputEvent;
@@ -23,8 +26,12 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 
 public class CharacterOverhaulHooks
 {
@@ -49,6 +56,48 @@ public class CharacterOverhaulHooks
 			}
 		}
 		return false;
+	}
+
+	@SubscribeEvent
+	/**
+	 * Ticks all Entities and TileEntities, as well as any ItemStacks they contain (recursively)
+	 */
+	public void tickCharacters(WorldTickEvent event)
+	{
+		if(event.phase == Phase.START && !event.world.isRemote)
+		{
+			for(Entity e : event.world.loadedEntityList)
+			{
+				tickCharacterEffects(e);
+			}
+			for(TileEntity tile : event.world.loadedTileEntityList)
+			{
+				tickCharacterEffects(tile);
+			}
+		}
+	}
+
+	/**
+	 * Recursively ticks all effects of this provider, and any items it contains
+	 * @param provider
+	 */
+	private void tickCharacterEffects(ICapabilityProvider provider)
+	{
+		if(provider.hasCapability(CapabilityCharacterHandler.CHARACTER_CAPABILITY, null))
+		{
+			for(ICharacterEffect effect : provider.getCapability(CapabilityCharacterHandler.CHARACTER_CAPABILITY, null).getEffects())
+			{
+				effect.tick();
+			}
+		}
+		if(provider.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null))
+		{
+			IItemHandler items = provider.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+			for(int i = 0; i < items.getSlots(); i++)
+			{
+				tickCharacterEffects(items.getStackInSlot(i));
+			}
+		}
 	}
 
 	@SubscribeEvent
