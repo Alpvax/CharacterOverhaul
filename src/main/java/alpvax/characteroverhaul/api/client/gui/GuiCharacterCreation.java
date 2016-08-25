7f00770c2bfbc4c1aeef2af86275456811a89b84
Alpvax <alpvax@netscape.net>
2016-08-25 16:15:08 +0100

    Added Creation GUI initialisation

--- src/main/java/alpvax/characteroverhaul/api/client/gui/GuiCharacterCreation.java
+++ src/main/java/alpvax/characteroverhaul/api/client/gui/GuiCharacterCreation.java
@@ -1,8 +1,11 @@
 package alpvax.characteroverhaul.api.client.gui;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.collect.ImmutableList;
+
+import alpvax.characteroverhaul.api.character.ICharacter;
+import alpvax.characteroverhaul.api.character.modifier.CharacterModifierFactory;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -10,6 +13,22 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class GuiCharacterCreation extends GuiScreen
 {
-	private List<ICharacterCreationPage> pages = new ArrayList<>();
+	private final List<ICharacterCreationPage> pages;
+	private final ICharacter character;
 	//TODO:Create Creation GUI
+
+	public GuiCharacterCreation(ICharacter character)
+	{
+		this.character = character;
+		ImmutableList.Builder<ICharacterCreationPage> b = ImmutableList.<ICharacterCreationPage>builder();
+		for(CharacterModifierFactory<?, ?> factory : CharacterModifierFactory.REGISTRY.getValues())
+		{
+			List<ICharacterCreationPage> list = factory.getPagesForGUI();
+			if(list != null && factory.isValidForCharacter(character))
+			{
+				b.addAll(list);
+			}
+		}
+		pages = b.build();
+	}
 }
