5764a8dab70f9e0cfff9144961d956572f4b56a8
Alpvax <development@alpvax.uk>
2019-06-20 17:40:37 +0100

    Made target of /attributes optional

--- src/main/java/alpvax/mc/characteroverhaul/command/AttributeCommand.java
+++ src/main/java/alpvax/mc/characteroverhaul/command/AttributeCommand.java
@@ -2,8 +2,11 @@ package alpvax.mc.characteroverhaul.command;
 
 import alpvax.mc.characteroverhaul.character.attribute.AttributeDisplayUtil;
 import alpvax.mc.characteroverhaul.character.attribute.SortedModifierMap;
+import com.google.common.collect.Sets;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
@@ -11,16 +14,28 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.text.StringTextComponent;
 
+import java.util.Set;
+
 public class AttributeCommand {
   public AttributeCommand(CommandDispatcher<CommandSource> dispatcher) {
     dispatcher.register(Commands.literal("attributes")
+        .executes(ctx -> execute(ctx))
         .then(Commands.argument("target", EntityArgument.entity())
-            //.suggests()
+            .requires(source -> source.hasPermissionLevel(1))
             .executes(ctx -> execute(ctx.getSource(), EntityArgument.getEntity(ctx, "target"), false))
             .then(Commands.argument("groupBySource", BoolArgumentType.bool())
                 .executes(ctx -> execute(ctx.getSource(), EntityArgument.getEntity(ctx, "target"), BoolArgumentType.getBool(ctx, "groupBySource")))
             )
         )
+    );
+  }
+
+  private static int execute(CommandContext<CommandSource> ctx, String... passedArgs) throws CommandSyntaxException {
+    Set<String> args = Sets.newHashSet(passedArgs);
+    return execute(
+        ctx.getSource(),
+        args.contains("target") ? EntityArgument.getEntity(ctx, "target") : ctx.getSource().asPlayer(),
+        args.contains("groupBySource") ? BoolArgumentType.getBool(ctx, "groupBySource") : false
     );
   }
 
